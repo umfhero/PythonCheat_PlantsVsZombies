96e32eb756409d19646e1d90ff68acd63afdf343
Majid <67478293+umfhero@users.noreply.github.com>
2026-03-10 03:51:24 +0000

    x

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from resolver import resolve_pointer_chain
 
 
 def main():
-    pm = pymem.Pymem("popcapgame1.exe")
     module_base = pymem.process.module_from_name(pm.process_handle, "popcapgame1.exe").lpBaseOfDll
 
     pointer_base = module_base + 0x0011BC8C
@@ -15,10 +14,11 @@ def main():
             final_address = resolve_pointer_chain(pm, pointer_base, offsets)
             value = pm.read_int(final_address)
             print("Suns:", value)
-            pm.write_int(final_address, int(input("New suns:")))
-        except:
-            print("Uh oh")
+            pm.write_int(final_address, int(input("New suns: ")))
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
+
 
